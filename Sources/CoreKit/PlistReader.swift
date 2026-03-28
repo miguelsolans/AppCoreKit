c36cef7c1bd87156731fe4b953d5ffc85f508b2c
Miguel Solans <cmiguelsolans@gmail.com>
2026-03-28 17:40:39 +0000

    Add PlistReader

---
+++ Sources/CoreKit/PlistReader.swift
@@ -0,0 +1,15 @@
+//
+//  PlistReader.swift
+//  CoreKit
+//
+//  Created by Miguel Solans on 28/03/2026.
+//
+
+import Foundation
+
+struct PlistReader {
+    
+    static func value<T>(forKey key: String) -> T? {
+        return Bundle.main.object(forInfoDictionaryKey: key) as? T
+    }
+}

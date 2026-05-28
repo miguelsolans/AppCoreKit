b225dddea8abe41a6218f07a0368e513194fb94b
Miguel Solans <cmiguelsolans@gmail.com>
2026-05-28 23:42:04 +0100

    Add top most viewcontroller

--- Sources/CoreKit/BaseViewController.swift
+++ Sources/CoreKit/BaseViewController.swift
@@ -111,3 +111,22 @@ open class BaseViewController: UIViewController {
     open func setupUI() {}
     open func setupBindings() {}
 }
+
+
+extension UIViewController {
+    public var topMostViewController: UIViewController {
+        if let presentedViewController {
+            return presentedViewController.topMostViewController
+        }
+
+        if let navigationController = self as? UINavigationController {
+            return navigationController.visibleViewController?.topMostViewController ?? navigationController
+        }
+
+        if let tabBarController = self as? UITabBarController {
+            return tabBarController.selectedViewController?.topMostViewController ?? tabBarController
+        }
+
+        return self
+    }
+}
